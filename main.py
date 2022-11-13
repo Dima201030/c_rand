8730a5e0b8d96d2c96f8d2889e8dec321ccc33af
Dima201030 <115942204+Dima201030@users.noreply.github.com>
2022-11-13 20:53:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+
+def rand(x):
+    random.number = random.randint(1, x)
+    rand = 0
+    while rand != random.number:
+        rand = int(input(f"какое число загадал компютер от 1 до {x}:"))
+        if rand < random.number:
+            print("не то, слишком мало")
+        elif rand > random.number:
+            print("ай ай ай, слишком много")
+        elif rand == random.number:
+            print(f"уху, молодец угадал {random.number}")
+rand(10)
+
